4ce8e8f313d9241e058348a5e99af051aa7e23a0
Itachixtanjiro <96478749+Itachixtanjiro@users.noreply.github.com>
2022-03-01 23:49:56 +0530

    Add files via upload

---
+++ assignment 4 output.py
@@ -0,0 +1,88 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======== RESTART: C:\Users\ASUS\OneDrive\Desktop\python\assignment 4.py ========
+
+ Question 1
+
+Enter the number : 3
+Move Disk 1 from S to T
+Move Disk 2 from S to Au
+Move Disk 1 from T to Au
+Move Disk 3 from S to T
+Move Disk 1 from Au to S
+Move Disk 2 from Au to T
+Move Disk 1 from S to T
+
+
+ Question 2
+
+Enter the number of rows in Pascal's Triangle: 5
+
+ Recursions Method:
+
+        1   
+      1   1   
+    1   2   1   
+  1   3   3   1   
+1   4   6   4   1   
+
+ Loops Method :
+
+        1   
+      1   1   
+    1   1   1   
+  1   1   1   1   
+1   1   1   1   1   
+
+
+ Question 3
+
+Enter the first integer: 32
+Enter the second integer: 23
+Remainder is:  9
+Quotient is:  1
+Both values are non zero
+{13}
+{5, 13}
+{5, 13, 14}
+{5, 13, 14, 6}
+{5, 6, 13, 14, 15}
+{5, 6, 7, 13, 14, 15}
+{5, 6, 7, 13, 14, 15}
+Immutable set:  frozenset({5, 6, 7, 13, 14, 15})
+Maximum value in the set:  15
+Hash value:  15
+
+
+ Question 4 
+
+Enter name of the student: PRIYANSHU
+Enter the sid of the student: 21105060
+Object created 
+
+The name of the student it PRIYANSHU and SID is 21105060.
+
+
+ Question 5 
+
+
+ (a) The updated salary of the employee Mehak is 70000 
+
+
+ (b) Employee Viren record deleted
+
+
+ Question 6 
+
+Barbie and George are here to play a game please help them!!!
+Enter the word George would say: okok
+
+Enter a new word that Barbie would think of to test your friendship: koko
+The letter aren't exact, friendship is fake!!
+
+Does the word makes sense?(y or n)
+y
+The friends pass the friendship test!!
+
+
